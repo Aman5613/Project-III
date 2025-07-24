49f1092b9e10174a78f0b81741c615885edff973
Aman5613 <161672923+Aman5613@users.noreply.github.com>
2025-07-25 02:23:39 +0530

    Reintialization of project again on 25 july adding axios config code

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,37 @@
+import axios from "axios";
+import { instance } from "./Utils/axios.config";
 import { useEffect } from "react";
-import instance from "./API/axiosconfig"
-import { asyncgetProducts } from "./Store/userAction";
+
+
 
 const App = () => {
-  useEffect(() => {
-    asyncgetProducts()
+  const getProduct = async () => {
+    try {
+      const response = await instance.get("/products")
+      console.log(response.data);
+      
+    } catch (error) {
+      console.log(error.message);
+      
+    }
+  }
+
+  const getUsers = async () => {
+    try {
+      const res = await instance.get("/users");
+      console.log(res.data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  useEffect( () => {
+    getProduct();
+    getUsers();
   }, [])
-
-
   return (
     <div>
-      
+      <h1>Hello from new world</h1>
     </div>
   )
 }
